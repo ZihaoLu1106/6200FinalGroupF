eaf800077016ab557b99a0711033c6d01bbef2e4
Jiawei Liao <145169410+002481222@users.noreply.github.com>
2023-12-13 17:46:44 -0500

    Update SingletonAdmin.java

--- 6200Final/src/DaysCare/SingletonAdmin.java
+++ 6200Final/src/DaysCare/SingletonAdmin.java
@@ -29,12 +29,12 @@ public class SingletonAdmin {
     static Map<Student,List<ImmunizationRecord>>studentMap;//each student has a list of thier list of immunizastion record
     static Map<Level,Integer>groupSizeMap;//it will store the size of each group in different level
     static Map<Level,Integer>classSizeMap;//it will store the size of each class in different level
-    
+
     static List<Student> studentList;//used to store all student
     static List<Teacher> teacherList;//used to store all teachers
     static List<Immunization> immunizationList;//use to store all immunizations
     private static final SingletonAdmin instance = new SingletonAdmin();
-    
+
     static int groupNum;
     static int classNum;
 
@@ -44,10 +44,10 @@ public class SingletonAdmin {
         studentMap=new HashMap<>();
         groupSizeMap=new HashMap<>();
         classSizeMap=new HashMap<>();
-        
+
         studentList = new ArrayList<>();
         teacherList = new ArrayList<>();
-        
+
         groupNum=0;
         classNum=0;
 
@@ -66,7 +66,7 @@ public class SingletonAdmin {
         initializeMap();
         //6.distribution student and teacher to each group and class
         distribute();
-        
+
 
         return instance;
     }
@@ -85,7 +85,7 @@ public class SingletonAdmin {
         List<String>records=FileUtil.readFIle("/Users/tunisabadbear/IdeaProjects/6200FinalGroupF/6200FinalGroupF/6200Final/src/DaysCare/Data/ImmunizationRecord.txt");
         //read student file and create all student object
         for(String s:students){
-            instance.studentList.add((Student) PersonFactory.STUDENT.getPerson(s));  
+            instance.studentList.add((Student) PersonFactory.STUDENT.getPerson(s));
         }
         //for each student,it will make a list of immunization list
         //then put the student and immunization to hash map studentMap
@@ -96,9 +96,9 @@ public class SingletonAdmin {
             //each line contins some date for last time immunization
             //and each date corresponds to one immunization
             for(int j=0;j<instance.immunizationList.size();j++){
-                
+
                 ImmunizationRecord ir=new ImmunizationRecord(instance.immunizationList.get(j),instance.studentList.get(i),a[j+1]);
-                
+
                 recordList.add(ir);
             }
             //put the student and his/her immunization record list to hashmap;
@@ -109,7 +109,7 @@ public class SingletonAdmin {
         List<String> teachers = FileUtil.readFIle("/Users/tunisabadbear/IdeaProjects/6200FinalGroupF/6200FinalGroupF/6200Final/src/DaysCare/Data/TeacherData.txt");
         //read teacher file and create all teacher object
         for(String s:teachers){
-            instance.teacherList.add((Teacher) PersonFactory.TEACHER.getPerson(s));  
+            instance.teacherList.add((Teacher) PersonFactory.TEACHER.getPerson(s));
         }
     }
     private static void readRatio() {
@@ -119,25 +119,25 @@ public class SingletonAdmin {
             String []a = s.split(",");
             Level level=Level.getType(Integer.parseInt(a[0]));
             int capacity=Integer.parseInt(a[2]);
-            
+
             instance.groupSizeMap.put(level, capacity);
         }
-        
+
         //read class size data
         List<String> classSizeData = FileUtil.readFIle("/Users/tunisabadbear/IdeaProjects/6200FinalGroupF/6200FinalGroupF/6200Final/src/DaysCare/Data/RatioRoleForGroupToClassRoom.txt");
         for(String s:classSizeData){
             String []a = s.split(",");
             Level level=Level.getType(Integer.parseInt(a[0]));
             int capacity=Integer.parseInt(a[2]);
-            
+
             instance.classSizeMap.put(level, capacity);
         }
-        
+
     }
-    
+
     private static void initializeMap(){
         for (Map.Entry<Level, Integer> classSizeset
-                                          : classSizeMap.entrySet()) {
+                : classSizeMap.entrySet()) {
             Group group=createGroup(groupSizeMap.get(classSizeset.getKey()));
             Classroom classroom=createClassroom(classSizeset.getValue());
             List<Classroom>tempClassroomList=new ArrayList<>();
@@ -145,8 +145,8 @@ public class SingletonAdmin {
             //
             levelMap.put(classSizeset.getKey(), tempClassroomList);
             classroom.addGroup(group);
-            
-            
+
+
         }
     }
     private static Group createGroup(int capacity){
@@ -160,7 +160,7 @@ public class SingletonAdmin {
         classNum++;
         return classroom;
     }
-    
+
     private static void distribute() {
         //distribute student to group with teacher
         for(Student s:studentList){
@@ -177,8 +177,8 @@ public class SingletonAdmin {
             List<Group>groupList=classMap.get(classroomList.get(0));
             //get the student's teacher list
             List<Teacher>teacherList=instance.teacherList;
-            
-            
+
+
             //find space
             //check if a class is full by group
             //search every last group in each classroom to check if has space left
@@ -208,7 +208,7 @@ public class SingletonAdmin {
                         //create new group
                         Group newGroup=createGroup(groupSizeMap.get(level));
                         tempClass.addGroup(newGroup);
-                       // classMap.get(tempClass).add(newGroup);
+                        // classMap.get(tempClass).add(newGroup);
                         targetClassroom=tempClass;
                         targetGroup=tempGroup;
                         isFindClass=true;
@@ -222,27 +222,27 @@ public class SingletonAdmin {
                     }
                 }
             }
-            
+
             if(isFindClass==false){//if all classroom is full, we need to create a new classroom
                 Classroom newClassroom=createClassroom(classSizeMap.get(level));//create new classroom
                 levelMap.get(level).add(newClassroom);
                 Group newGroup=createGroup(groupSizeMap.get(level));//create new group
                 newClassroom.addGroup(newGroup);
                 //classMap.put(newClassroom,newClassroom.getGourpList());
-                
+
                 targetClassroom=newClassroom;
                 targetGroup=newGroup;
             }
-            
+
             //now we get the classroom and group we need
             //put the student into the group;
-            
+
             if(targetGroup.addStudent(s)){
                 System.out.println("Success student "+s+" group:"+targetGroup.getGourpNum()+" class:"+targetClassroom.getClassNum());
             }
+        }
     }
-}
-//This method is going to write the immunization record when student take shot
+    //This method is going to write the immunization record when student take shot
     public static void updateImmunizationRecordFile() {
         List<String> linesToWrite = new ArrayList<>();
         for (Student s : studentList) {
@@ -257,11 +257,11 @@ public class SingletonAdmin {
         FileUtil.writeToFile("/Users/tunisabadbear/IdeaProjects/6200FinalGroupF/6200FinalGroupF/6200Final/src/DaysCare/Data/ImmunizationRecord.txt", linesToWrite);
     }
 
-    
-    
-    
-    
-    
+
+
+
+
+
 
     public static Map<Level, List<Classroom>> getLevelMap() {
         return levelMap;
@@ -294,6 +294,6 @@ public class SingletonAdmin {
     public static List<Immunization> getImmunizationList() {
         return immunizationList;
     }
-    
+
 
 }
